73fd1ab91b001acb623d8e333c3a403aa8e0e684
Zibo Camito <zibo.camito@gmail.com>
2020-07-16 16:04:29 +0200

    add InputSystem logics

--- zengine/src/basic/input.rs
+++ zengine/src/basic/input.rs
@@ -1,12 +1,17 @@
+use crate::core::system::Read;
 use crate::core::system::System;
+use crate::core::system::Write;
+use crate::core::Resource;
+use crate::core::Store;
 use crate::event::event::Input;
-use crate::event::keyboard::Key;
+use crate::event::event::InputEvent;
+use crate::event::event_stream::{EventStream, SubscriptionToken};
 use fnv::FnvHashMap;
 use serde::Deserialize;
 use std::any::Any;
 use std::hash::Hash;
 
-pub trait InputType: Any + Eq + PartialEq + Hash {}
+pub trait InputType: Any + Eq + PartialEq + Hash + Clone {}
 
 impl InputType for String {}
 
@@ -23,32 +28,92 @@ pub struct Action {
 
 #[derive(Default, Deserialize, PartialEq)]
 pub struct Bindings<T: InputType> {
-    pub axes: FnvHashMap<T, Vec<Axis>>,
-    pub actions: FnvHashMap<T, Vec<Action>>,
+    pub axis_mappings: FnvHashMap<T, Vec<Axis>>,
+    pub action_mappings: FnvHashMap<T, Vec<Action>>,
 }
 
 #[derive(Default)]
 pub struct InputSystem<T: InputType> {
+    input_stream_token: Option<SubscriptionToken>,
     bindings: Bindings<T>,
 }
 
+pub struct InputHandler<T: InputType> {
+    actions_value: FnvHashMap<T, f32>,
+    axes_value: FnvHashMap<T, f32>,
+}
+impl<T: InputType> Default for InputHandler<T> {
+    fn default() -> Self {
+        InputHandler {
+            actions_value: FnvHashMap::default(),
+            axes_value: FnvHashMap::default(),
+        }
+    }
+}
+
+impl<T: InputType> Resource for InputHandler<T> {}
+
 impl<T: InputType> InputSystem<T> {
     pub fn from_bindings(bindings: Bindings<T>) -> Self {
-        InputSystem { bindings: bindings }
+        InputSystem {
+            input_stream_token: None,
+            bindings: bindings,
+        }
     }
 }
 
 impl<'a, T: InputType> System<'a> for InputSystem<T> {
-    type Data = ();
+    type Data = (
+        Read<'a, EventStream<InputEvent>>,
+        Write<'a, InputHandler<T>>,
+    );
 
-    fn run(&mut self, data: Self::Data) {}
+    fn init(&mut self, store: &mut Store) {
+        if let Some(mut input_stream) = store.get_resource_mut::<EventStream<InputEvent>>() {
+            self.input_stream_token = Some(input_stream.subscribe());
+        }
+
+        store.insert_resource(InputHandler::<T>::default());
+    }
+
+    fn run(&mut self, (event_stream, mut input_handler): Self::Data) {
+        if let Some(token) = self.input_stream_token {
+            for e in event_stream.read(&token) {
+                for actions in self.bindings.action_mappings.iter() {
+                    if let Some(_action) = actions.1.iter().find(|action| action.source == e.input)
+                    {
+                        input_handler
+                            .actions_value
+                            .insert(actions.0.clone(), e.value);
+                    }
+                }
+                for axes in self.bindings.axis_mappings.iter() {
+                    if let Some(axis) = axes.1.iter().find(|axis| axis.source == e.input) {
+                        input_handler
+                            .axes_value
+                            .insert(axes.0.clone(), e.value * axis.scale);
+                    }
+                }
+            }
+        }
+    }
+
+    fn dispose(&mut self, store: &mut Store) {
+        if let Some(token) = self.input_stream_token {
+            if let Some(mut input_stream) = store.get_resource_mut::<EventStream<InputEvent>>() {
+                input_stream.unsubscribe(token);
+                self.input_stream_token = None;
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::event::keyboard::Key;
 
-    #[derive(Eq, PartialEq, Hash, Deserialize)]
+    #[derive(Eq, PartialEq, Hash, Deserialize, Clone)]
     enum UserInput {
         Jump,
         X,
@@ -59,7 +124,7 @@ mod tests {
     #[test]
     fn decode_typed_bindings_from_yaml() {
         let content = "
-            axes: 
+            axis_mappings: 
                 X:
                 - source:
                     Keyboard:
@@ -69,7 +134,7 @@ mod tests {
                     Keyboard:
                         key: A
                   scale: -1.0
-            actions:
+            action_mappings:
                 Jump:
                 - source: 
                     Keyboard: 
@@ -79,14 +144,14 @@ mod tests {
         let bindings: Bindings<UserInput> = serde_yaml::from_str(&content).unwrap();
 
         assert_eq!(
-            bindings.actions.get(&UserInput::Jump).unwrap(),
+            bindings.action_mappings.get(&UserInput::Jump).unwrap(),
             &vec!(Action {
                 source: Input::Keyboard { key: Key::Space }
             })
         );
 
         assert_eq!(
-            bindings.axes.get(&UserInput::X).unwrap(),
+            bindings.axis_mappings.get(&UserInput::X).unwrap(),
             &vec!(
                 Axis {
                     source: Input::Keyboard { key: Key::D },
@@ -105,7 +170,7 @@ mod tests {
     #[test]
     fn decode_generics_bindings_from_yaml() {
         let content = "
-            axes: 
+            axis_mappings: 
                 X:
                 - source:
                     Keyboard:
@@ -115,7 +180,7 @@ mod tests {
                     Keyboard:
                         key: A
                   scale: -1.0
-            actions:
+            action_mappings:
                 Jump:
                 - source: 
                     Keyboard: 
@@ -125,14 +190,14 @@ mod tests {
         let bindings: Bindings<String> = serde_yaml::from_str(&content).unwrap();
 
         assert_eq!(
-            bindings.actions.get("Jump").unwrap(),
+            bindings.action_mappings.get("Jump").unwrap(),
             &vec!(Action {
                 source: Input::Keyboard { key: Key::Space }
             })
         );
 
         assert_eq!(
-            bindings.axes.get("X").unwrap(),
+            bindings.axis_mappings.get("X").unwrap(),
             &vec!(
                 Axis {
                     source: Input::Keyboard { key: Key::D },
@@ -148,27 +213,3 @@ mod tests {
         InputSystem::from_bindings(bindings);
     }
 }
-/*
-
-{
-  "axis": {
-    "move_y": [
-       { "source": "W", "scale": 1.0 },
-       { "source": "S", "scale": -1.0 },
-       { "source": "Controller1.LeftStick.Y", "scale": 1.0 }
-    ],
-    "move_x": [
-       { "source": "D", "scale": 1.0 },
-       { "source": "A", "scale": -1.0 },
-       { "source": "Controller1.LeftStick.X", "scale": 1.0 }
-    ]
-  },
-  "actions": {
-    "jump": [
-       { "source": "SPACEBAR", "mod": "(something like shift, ctrl, alt, etc..." },
-       { "source": "Controller1.Button.A" }
-    ]
-  }
-}
-
-*/
