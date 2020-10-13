3aebbba604466383eeae6bd0c4d8e5e19497eb4b
Zibo Camito <zibo.camito@gmail.com>
2020-10-13 09:26:16 +0200

    first complete working example

--- zengine/src/core/join.rs
+++ zengine/src/core/join.rs
@@ -1,132 +1,183 @@
+use crate::core::component::AnySet;
 use crate::core::component::Component;
 use crate::core::component::Set;
 use crate::core::entity::Entity;
 use std::collections::hash_map::Iter;
 use std::collections::hash_map::IterMut;
+use std::collections::hash_map::Keys;
+use std::marker::PhantomData;
 
-pub enum JoinComponentReturn<D> {
+enum JoinReturn<T> {
     Skip,
-    Value(D),
+    Value(T),
 }
 
-pub struct JoinIterator<I, D> {
-    iter: I,
-    other: D,
+trait Joined {
+    type Output;
+
+    fn get_join_value(&mut self, entity: &Entity) -> JoinReturn<Self::Output>;
 }
 
-pub trait Joined {
-    type Iter;
-    type Item;
+impl<'a, C: Component> Joined for &'a Set<C> {
+    type Output = &'a C;
 
-    fn iter(self) -> Self::Iter;
-
-    fn entity_component(self, entity: &Entity) -> JoinComponentReturn<Self::Item>;
-}
-
-type JoinedSet<'a, C: Component> = &'a Set<C>;
-type JoinedSetMut<'a, C: Component> = &'a mut Set<C>;
-
-impl<'a, C: Component> Joined for JoinedSet<'a, C> {
-    type Iter = Iter<'a, Entity, C>;
-    type Item = &'a C;
-
-    fn iter(self) -> Self::Iter {
-        self.iter()
-    }
-
-    fn entity_component(self, entity: &Entity) -> JoinComponentReturn<Self::Item> {
+    fn get_join_value(&mut self, entity: &Entity) -> JoinReturn<Self::Output> {
         match self.get(entity) {
-            Some(component) => JoinComponentReturn::Value(component),
-            None => JoinComponentReturn::Skip,
+            Some(component) => JoinReturn::Value(component),
+            None => JoinReturn::Skip,
         }
     }
 }
 
-impl<'a, C: Component> Joined for JoinedSetMut<'a, C> {
-    type Iter = IterMut<'a, Entity, C>;
-    type Item = &'a mut C;
+impl<'a, C: Component> Joined for &'a mut Set<C> {
+    type Output = &'a mut C;
 
-    fn iter(self) -> Self::Iter {
-        self.iter_mut()
-    }
-
-    fn entity_component(self, entity: &Entity) -> JoinComponentReturn<Self::Item> {
-        match self.get_mut(entity) {
-            Some(component) => JoinComponentReturn::Value(component),
-            None => JoinComponentReturn::Skip,
+    fn get_join_value(&mut self, entity: &Entity) -> JoinReturn<Self::Output> {
+        //SAFETY FIXME write something that explains why this unsafe code
+        //is actually safe
+        unsafe {
+            match self.get_mut(entity) {
+                Some(component) => JoinReturn::Value(&mut *(component as *mut C)),
+                None => JoinReturn::Skip,
+            }
         }
     }
 }
 
-impl<'a, A: Component, B: Joined + Copy> Iterator for JoinIterator<Iter<'a, Entity, A>, B> {
-    type Item = (&'a Entity, &'a A, B::Item);
+trait Joinable {}
+impl<T: Joined> Joinable for T {}
+
+trait Join<C: Component, D> {
+    fn join(&self, joined: D) -> JoinIter<Iter<Entity, C>, D>;
+
+    fn join_mut(&mut self, joined: D) -> JoinIter<IterMut<Entity, C>, D>;
+}
+
+impl<Comp: Component, D: Joinable> Join<Comp, D> for Set<Comp> {
+    fn join(&self, joined: D) -> JoinIter<Iter<Entity, Comp>, D> {
+        JoinIter {
+            iter: self.iter(),
+            joined: joined,
+        }
+    }
+
+    fn join_mut(&mut self, joined: D) -> JoinIter<IterMut<Entity, Comp>, D> {
+        JoinIter {
+            iter: self.iter_mut(),
+            joined: joined,
+        }
+    }
+}
+
+macro_rules! impl_join_for_tuple {
+    ( $($ty:ident),* ) => {
+        impl<Comp: Component, $($ty),*> Join<Comp, ( $( $ty, )* )> for Set<Comp>
+            where $( $ty: Joinable ),*
+            {
+                fn join(&self, joined: ( $( $ty, )* )) -> JoinIter<Iter<Entity, Comp>, ( $( $ty, )* )> {
+                    JoinIter {
+                        iter: self.iter(),
+                        joined: joined,
+                    }
+                }
+                fn join_mut(&mut self, joined: ( $( $ty, )* )) -> JoinIter<IterMut<Entity, Comp>, ( $( $ty, )* )> {
+                    JoinIter {
+                        iter: self.iter_mut(),
+                        joined: joined,
+                    }
+                }
+            }
+        }
+}
+impl_join_for_tuple!(A, B);
+impl_join_for_tuple!(A, B, C);
+impl_join_for_tuple!(A, B, C, D);
+impl_join_for_tuple!(A, B, C, D, E);
+impl_join_for_tuple!(A, B, C, D, E, F);
+impl_join_for_tuple!(A, B, C, D, E, F, G);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y);
+impl_join_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z);
+
+struct JoinIter<I: Iterator, D> {
+    iter: I,
+    joined: D,
+}
+
+impl<'a, A: 'a + Component, B: 'a + Joined> Iterator for JoinIter<Iter<'a, Entity, A>, B> {
+    type Item = (&'a Entity, &'a A, B::Output);
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut next_value: Option<Self::Item> = None;
-
         while let Some(entry) = self.iter.next() {
-            let c1 = match self.other.entity_component(entry.0) {
-                JoinComponentReturn::Skip => {
-                    break;
-                }
-                JoinComponentReturn::Value(component) => component,
-            };
+            next_value = Some((
+                entry.0,
+                entry.1,
+                match self.joined.get_join_value(entry.0) {
+                    JoinReturn::Value(other) => other,
+                    JoinReturn::Skip => break,
+                },
+            ));
 
-            next_value = Some((entry.0, entry.1, c1));
-            break;
+            if next_value.is_some() {
+                break;
+            }
         }
 
         next_value
     }
 }
 
-impl<'a, A: Component, B: Joined + Copy> Iterator for JoinIterator<IterMut<'a, Entity, A>, B> {
-    type Item = (&'a Entity, &'a mut A, B::Item);
+macro_rules! impl_join_iterator_for_tuple {
+    ( $($ty:ident => $index:tt),* ) => {
+        impl<'a, Comp, $($ty),*> Iterator
+            for JoinIter<Iter<'a, Entity, Comp>, ( $( $ty, )* )>
+            where Comp: 'a + Component, $( $ty: 'a + Joined ),*
+        {
+            type Item = (&'a Entity, &'a Comp, $( $ty::Output, )* );
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut next_value: Option<Self::Item> = None;
+            fn next(&mut self) -> Option<Self::Item> {
+                let mut next_value: Option<Self::Item> = None;
+                while let Some(entry) = self.iter.next() {
+                    next_value = Some((
+                        entry.0,
+                        entry.1,
+                        $(
+                            match self.joined.$index.get_join_value(entry.0) {
+                                JoinReturn::Value(other) => other,
+                                JoinReturn::Skip => break,
+                            }
+                        ), *
+                    ));
 
-        while let Some(entry) = self.iter.next() {
-            let c1 = match self.other.entity_component(entry.0) {
-                JoinComponentReturn::Skip => {
-                    break;
+                    if next_value.is_some() {
+                        break;
+                    }
                 }
-                JoinComponentReturn::Value(component) => component,
-            };
 
-            next_value = Some((entry.0, entry.1, c1));
-            break;
-        }
-
-        next_value
-    }
-}
-
-pub trait Joinable<I, D> {
-    fn join(self) -> JoinIterator<I, D>;
-}
-
-impl<'a, A: Component, B: Component> Joinable<Iter<'a, Entity, A>, JoinedSet<'a, B>>
-    for (JoinedSet<'a, A>, JoinedSet<'a, B>)
-{
-    fn join(self) -> JoinIterator<Iter<'a, Entity, A>, JoinedSet<'a, B>> {
-        JoinIterator {
-            iter: self.0.iter(),
-            other: self.1,
+                next_value
+            }
         }
     }
 }
-
-impl<'a, A: Component, B: Component> Joinable<IterMut<'a, Entity, A>, JoinedSet<'a, B>>
-    for (JoinedSetMut<'a, A>, JoinedSet<'a, B>)
-{
-    fn join(self) -> JoinIterator<IterMut<'a, Entity, A>, JoinedSet<'a, B>> {
-        JoinIterator {
-            iter: self.0.iter_mut(),
-            other: self.1,
-        }
-    }
-}
+impl_join_iterator_for_tuple!(A => 0, B => 1);
+//impl_join_iterator_for_tuple!(A => [0], [mut], B => [1], [mut], C => [2], [mut]);
 
 #[cfg(test)]
 mod tests {
@@ -218,27 +269,54 @@ mod tests {
 
         (storage1, storage2, storage3)
     }
-
     #[test]
     fn join_iterator() {
-        let (storage1, storage2, storage3) = prapare_join_test();
+        let (mut storage1, mut storage2, mut storage3) = prapare_join_test();
 
-        assert_eq!((&storage1, &storage2).join().count(), 2);
+        let join_iter = storage1.join(&storage2);
+        let join_iter = storage1.join(&mut storage2);
+        let join_iter = storage1.join((&storage2, &storage3));
+        let join_iter = storage1.join((&mut storage2, &storage3));
+        let join_iter = storage1.join((&storage2, &mut storage3));
+        let join_iter = storage1.join((&mut storage2, &mut storage3));
 
-        for (entity, c1, c2) in (&storage1, &storage2).join() {
+        assert_eq!(storage1.join(&storage2).count(), 2);
+
+        for (entity, c1, c2) in storage1.join(&storage2) {
             println!("{:?}", c1.data1);
         }
+
+        assert_eq!(storage1.join((&storage2, &storage3)).count(), 2);
+
+        for (entity, c1, c2, c3) in storage1.join((&storage2, &storage3)) {
+            println!("{:?}", c1.data1);
+        }
+
+        for (entity, c1, mut c2) in storage1.join(&mut storage2) {
+            println!("{:?}", c1.data1);
+            c2.data4 = 7;
+        }
+
+        for (entity, c1, c2, c3) in storage1.join((&storage2, &storage3)) {
+            println!("{:?}", c1.data1);
+        }
+
+        //let test = storage1.join(5);
     }
+
+    /*
     #[test]
     fn join_iterator_mut() {
         let (mut storage1, mut storage2, mut storage3) = prapare_join_test();
 
-        assert_eq!((&mut storage1, &storage2).join().count(), 2);
+        //assert_eq!((&mut storage1, &storage2, &mut storage3).join(), 2);
 
-        for (entity, mut c1, c2) in (&mut storage1, &storage2).join() {
+        let test = (&mut storage1, &storage2, &mut storage3).join();
+
+        for (entity, mut c1, c2, mut c3) in (&mut storage1, &storage2, &mut storage3).join() {
             println!("{:?}", c1.data1);
             c1.data1 = 5;
-            //c2.data4 = 7;
+            c3.data5 = 7;
         }
-    }
+    }*/
 }
